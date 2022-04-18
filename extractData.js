4653526a079fa13c6d18d62bc8e50c4b272a56ad
awesomehaze <>
2022-04-17 19:25:17 -0700

    finish implementing schedule fetch

--- extractData.js
+++ extractData.js
@@ -1,5 +1,5 @@
 window.addEventListener("message", function (event) {
-    if (event.data.settingsChanged) {
+    if(event.data.settingsChanged) {
         chrome.storage.local.set({
             settingsJSON: localStorage.getItem("settings")
         }, function () {});
