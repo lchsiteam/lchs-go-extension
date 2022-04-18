8e115ff6c7e4ba9903f5898d2b4877c60cbed815
awesomehaze <>
2022-04-17 19:01:56 -0700

    remove log

--- extractData.js
+++ extractData.js
@@ -1,11 +1,7 @@
 window.addEventListener("message", function (event) {
     if (event.data.settingsChanged) {
-        chrome.storage.local.set({ settingsJSON: localStorage.getItem("settings") }, function () {
-        });
-
-        chrome.storage.local.get(['settingsJSON'], function (result) {
-            console.log('Value currently is ' + result.settingsJSON);
-        });
-        console.log(JSON.parse(localStorage.getItem("settings")))
+        chrome.storage.local.set({
+            settingsJSON: localStorage.getItem("settings")
+        }, function () {});
     }
 });
